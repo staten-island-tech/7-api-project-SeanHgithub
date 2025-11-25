f650a17eda1381b1d48e912cd0270124f8777205
SeanHgithub <seanhadley11@gmail.com>
2025-11-25 10:16:36 -0500

    api

--- sean.py
+++ sean.py
@@ -1,19 +1,18 @@
-from decimal import Decimal, getcontext
-
-def arctan_series(x, iterations):
-    getcontext().prec = iterations * 2  # Set precision higher for intermediate calculations
-    term = Decimal(x)
-    sum_val = term
-    for i in range(1, iterations):
-        term = term * (-x*x) * (2*i - 1) / (2*i + 1)
-        sum_val += term
-    return sum_val
-
-def calculate_pi_machin(num_digits, iterations_per_arctan):
-    getcontext().prec = num_digits + 5 # Add buffer for precision
+import requests
+def getPoke(poke):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
     
-    arctan_1_5 = arctan_series(Decimal(1)/5, iterations_per_arctan)
-    arctan_1_239 = arctan_series(Decimal(1)/239, iterations_per_arctan)
-    
-    pi_val = 16 * arctan_1_5 - 4 * arctan_1_239
-    return pi_val
+    data = response.json()
+    return {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": [t["type"]["name"] for t in data["types"]]
+    }
+pokemon = getPoke("Bulbasaur")
+
+for key, value in pokemon.items():
+    print(f"{key.title()}: {value}")
