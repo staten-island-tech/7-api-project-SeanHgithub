cabf14e524a124c80cb1f144dfcb384bf086c16c
SeanHgithub <seanhadley11@gmail.com>
2025-12-10 10:17:18 -0500

    api

--- MLB.py
+++ MLB.py
@@ -1,12 +1,14 @@
 import requests
-import asyncio
-import fortnite_api
 
-async def main():
-    async with fortnite_api.Client(api_key="50ae7ff9-459e-4dbc-b8f2-b6d56e19062c") as client:
-        stats = await client.fetch_br_stats(name="Ship")
-        print(stats)
+url = "https://marvel-snap-api.p.rapidapi.com/api/get-all-cards"
 
-if __name__ == "__main__":
-    asyncio.run(main())
+querystring = {"page":"1"}
 
+headers = {
+	"x-rapidapi-key": "fb2044aa18msh62f0ab07b46ac36p19f523jsn9f224fb4edd3",
+	"x-rapidapi-host": "marvel-snap-api.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.json())
