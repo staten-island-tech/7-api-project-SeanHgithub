e9e92c81c180f114d09b0552840942068e31f3d1
SeanHgithub <seanhadley11@gmail.com>
2025-12-09 22:15:36 -0500

    Update MLB.py

--- MLB.py
+++ MLB.py
@@ -3,9 +3,10 @@ import asyncio
 import fortnite_api
 
 async def main():
-   async with fortnite_api.Client(api_key="your_api_key"):
-      stats = await client.fetch_br_stats(name='some_username')
-      print(stats)
+    async with fortnite_api.Client(api_key="50ae7ff9-459e-4dbc-b8f2-b6d56e19062c") as client:
+        stats = await client.fetch_br_stats(name="Ship")
+        print(stats)
 
 if __name__ == "__main__":
-   asyncio.run(main())
+    asyncio.run(main())
+
