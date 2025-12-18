82e24120d18c2fa343f8d9d71c7438dbd93aa9a0
SeanHgithub <seanhadley11@gmail.com>
2025-12-18 10:17:26 -0500

    Update MLB.py

--- MLB.py
+++ MLB.py
@@ -6,48 +6,53 @@ window.geometry("1000x500")
 
 label = tk.Label(
     window,
-    text="Hello, Tkinter!",
+    text="Word Search",
     bg="lightblue",
     fg="black",
     font=("Arial", 14),
-    padx=10
+    padx=10,
     pady=10
 )
+
 
 label.pack(
     side="top",
     pady=10
 )
 
-button = tk.Button(
-    window,
-    text="Click Me",
-    command=lambda: print("Button clicked!")
-)
+entry = tk.Entry(window)
+entry.pack(pady=10)
 
-button.pack(
-    side="bottom",
-    pady=10
-)
+def show_def():
+    def getword(word):
+        response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word.lower()}")
+        if response.status_code != 200:
+            window.title("Error Fetching Data!")
+            return None
+        data = response.json()
+        window.title("Word Found!")
+        word_info =  
+                "word": data[0]["word"],
+                "definitions": [
+                d["definition"]
+                for m in data[0]["meanings"]
+                for d in m["definitions"]
+                ]
+            }
+        output.pack()
+        text = entry.get()
+        word = getword(text)
+    
+    
+
+
+button = tk.Button(window, text="Submit", command=show_def)
+button.pack()
+
 window.mainloop()
-def getword(word):
-    response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word.lower()}")
-    if response.status_code != 200:
-        print("Error fetching data!")
-        return None
-    
-    
-    data = response.json()
-    return {
-        "word": data[0]["word"],
-        "definitions": [
-        d["definition"]
-        for m in data[0]["meanings"]
-        for d in m["definitions"]
-        ]
-    }
-word = getword("banana")
 
-for key, value in word.items():
-    print(f"{key.title()}: {value}")
+
+
+""" for key, value in word.items():
+    print(f"{key.title()}: {value}") """
 
