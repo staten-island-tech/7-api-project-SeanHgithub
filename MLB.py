40655b7fdf7f162c083939d19b49239d8c444a8c
SeanHgithub <seanhadley11@gmail.com>
2025-12-18 21:13:02 -0500

    Update MLB.py

--- MLB.py
+++ MLB.py
@@ -23,36 +23,40 @@ label.pack(
 entry = tk.Entry(window)
 entry.pack(pady=10)
 
-def show_def():
-    def getword(word):
-        response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word.lower()}")
-        if response.status_code != 200:
-            window.title("Error Fetching Data!")
-            return None
-        data = response.json()
-        window.title("Word Found!")
-        word_info =  
-                "word": data[0]["word"],
-                "definitions": [
-                d["definition"]
-                for m in data[0]["meanings"]
-                for d in m["definitions"]
-                ]
-            }
-        output.pack()
-        text = entry.get()
-        word = getword(text)
-    
-    
-
-
-button = tk.Button(window, text="Submit", command=show_def)
+def getword():
+    search = entry.get()
+    response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{search.lower()}")
+    if response.status_code != 200:
+        window.title("Error Fetching Data!")
+        return None
+    data = response.json()
+    window.title("Word Found!")
+    word =  {
+        "word": data[0]["word"],
+        "definitions": [
+         d["definition"]
+         for m in data[0]["meanings"]
+         for d in m["definitions"]
+         ]
+    }
+    organize_word = [
+        (f"{key.title()}: {value}")
+        for key, value in word.items()
+    ]
+    output = tk.Label(
+        window,
+        text="\n".join(organize_word),
+        bg="lightblue",
+        fg="black",
+        font=("Arial", 14),
+        padx=10,
+        pady=10,
+        wraplength=900,
+        justify="left"
+    )
+    output.pack()
+button = tk.Button(window, text="Submit", command=getword)
 button.pack()
 
 window.mainloop()
-
-
-
-""" for key, value in word.items():
-    print(f"{key.title()}: {value}") """
 
